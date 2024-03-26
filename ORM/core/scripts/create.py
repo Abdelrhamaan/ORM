e2dc932dbe37c5a82314218466e29903818823e7
Abdelrhman Mamdouh <a.mammdouh97@gmail.com>
2024-03-27 00:33:19 +0200

    f expression with filter, annotate, aggregate, compare and update on data base level

---
+++ ORM/core/scripts/create.py
@@ -0,0 +1,22 @@
+from core.models import Rating, Restaurant, Sale
+from django.contrib.auth.models import User
+import random
+from datetime import datetime
+# res_list = [1, 2, 3, 4, 5, 6]
+
+
+def run():
+    # for _ in range(30):
+    #     Rating.objects.create(
+    #         user=User.objects.get(pk=1),
+    #         restauarant=Restaurant.objects.get(pk=random.randint(1, 6)),
+    #         rating=random.randint(1, 5)
+    #     )
+
+    for _ in range(30):
+        Sale.objects.create(
+            income=random.randint(50000, 100000),
+            restaurant=Restaurant.objects.get(pk=random.randint(1, 6)),
+            expenditure=0,
+            datatime=datetime.now()
+        )
